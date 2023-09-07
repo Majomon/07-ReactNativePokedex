bf0ea1eedf4939c25b2c0a005292d1e4fc27fa5a
Mauri <mauri.monzon91@gmail.com>
2023-09-07 14:36:15 -0300

    act

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Image, Text, View} from 'react-native';
+import {FlatList, Image, Text, View} from 'react-native';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import {stylesGlobal} from '../theme/appTheme';
 import {usePokemonPaginated} from '../hooks/usePokemonPaginated';
@@ -8,21 +8,30 @@ export const HomeScreen = () => {
   const {top} = useSafeAreaInsets();
   const {isLoading, simplePokemonList} = usePokemonPaginated();
   console.log(simplePokemonList);
-  
+
   return (
     <View>
       <Image
         source={require('../assets/pokebola.png')}
         style={stylesGlobal.pokebolaBG}
       />
-      <Text
+      <FlatList
+        data={simplePokemonList}
+        keyExtractor={pokemon => pokemon.id}
+        renderItem={({item}) => (
+          <Text style={{color: 'black'}}>{item.name}</Text>
+        )}
+        //Scroll infinito
+        onEndReached={}
+      />
+      {/*       <Text
         style={{
           ...stylesGlobal.title,
           ...stylesGlobal.globalMargin,
           top: top + 20,
         }}>
         Pokedex
-      </Text>
+      </Text> */}
     </View>
   );
 };
