cc73f46ef9364ce8d66568a2b7dc83f1077d4bb5
Mauri <mauri.monzon91@gmail.com>
2023-09-07 13:08:18 -0300

    Filtrando informarion de la peticion a la Api

--- src/hooks/usePokemonPaginated.tsx
+++ src/hooks/usePokemonPaginated.tsx
@@ -1,12 +1,27 @@
-import {useEffect, useRef} from 'react';
+import {useEffect, useRef, useState} from 'react';
 import {pokemonApi} from '../api/PokemonApi';
+import {
+  PokemonPaginatedResponse,
+  Result,
+  SimplePokemon,
+} from '../interfaces/pokemonInterfaces';
 
 export const usePokemonPaginated = () => {
+  const [simplePokemonList, setSimplePokemonList] = useState<SimplePokemon[]>(
+    [],
+  );
   const nextPageUrl = useRef('https://pokeapi.co/api/v2/pokemon?limit=40');
 
   const loadPokemon = async () => {
-    const res = await pokemonApi.get(nextPageUrl.current);
-    console.log(res);
+    const res = await pokemonApi.get<PokemonPaginatedResponse>(
+      nextPageUrl.current,
+    );
+    nextPageUrl.current = res.data.next;
+    mapPokemonList(res.data.results);
+  };
+
+  const mapPokemonList = (pokemonList: Result[]) => {
+    pokemonList.map(poke => console.log(poke.name));
   };
 
   useEffect(() => {
