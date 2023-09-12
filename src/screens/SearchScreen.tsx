8f76cd477f4d89221e69907d5d797a9781ca1163
Mauri <mauri.monzon91@gmail.com>
2023-09-12 20:40:30 -0300

    Trayendo a todos los pokemon en una sola petición

--- src/screens/SearchScreen.tsx
+++ src/screens/SearchScreen.tsx
@@ -1,10 +1,25 @@
-import {View, Text} from 'react-native';
+import {View, Text, FlatList} from 'react-native';
 import React from 'react';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import {SearchInput} from '../components/SearchInput';
+import {usePokemonSearch} from '../hooks/usePokemonSearch';
+import {ActivityIndicator} from 'react-native';
+import {StyleSheet} from 'react-native';
+import {stylesGlobal} from '../theme/appTheme';
+import {PokemonCard} from '../components/PokemonCard';
 
 export const SearchScreen = () => {
   const {top} = useSafeAreaInsets();
+  const {isFetching, simplePokemonList} = usePokemonSearch();
+
+  if (isFetching) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size={50} color="grey" />
+        <Text style={styles.textLoading}>Cargando...</Text>
+      </View>
+    );
+  }
   return (
     <View
       style={{
@@ -15,6 +30,40 @@ export const SearchScreen = () => {
       }}>
       {/* Buscador */}
       <SearchInput />
+
+      <FlatList
+        data={simplePokemonList}
+        keyExtractor={pokemon => pokemon.id}
+        //Quitando barra vertical
+        showsVerticalScrollIndicator={false}
+        //Número de columnas de como se va a mostrar
+        numColumns={2}
+        //Header
+        ListHeaderComponent={
+          <Text
+            style={{
+              ...stylesGlobal.title,
+              ...stylesGlobal.globalMargin,
+
+              paddingBottom: 10,
+            }}>
+            Pokedex
+          </Text>
+        }
+        renderItem={({item}) => <PokemonCard pokemon={item} />}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  textLoading: {
+    color: 'black',
+    fontSize: 15,
+  },
+});
